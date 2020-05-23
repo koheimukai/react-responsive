4a0ea3d5a09ae6a93ffa070b26f799b45a6d31d8
Kohei Mukai <info@koheimukai.com>
2020-05-23 15:22:26 +0900

    Fixed bug on src/index.js

--- src/index.js
+++ src/index.js
@@ -10,37 +10,40 @@ import React, {
 const Responsive = createContext(null);
 Responsive.displayName = "Responsive";
 
-const defaultOption = { sm: 600, md: 960, lg: 1280, xl: 1920 };
+const defaultBreakpoints = { sm: 600, md: 960, lg: 1280, xl: 1920 };
 
-export function ResponsiveProvider({ children, option = defaultOption }) {
+export function ResponsiveProvider({ children, customBreakpoints }) {
+  const breakpoints = Object.assign({}, defaultBreakpoints, customBreakpoints);
+
   const [matches, setMatches] = useState({
-    sm: window.matchMedia(`(min-width: ${option.sm}px)`).matches,
-    md: window.matchMedia(`(min-width: ${option.md}px)`).matches,
-    lg: window.matchMedia(`(min-width: ${option.lg}px)`).matches,
-    xl: window.matchMedia(`(min-width: ${option.xl}px)`).matches,
+    sm: window.matchMedia(`(min-width: ${breakpoints.sm}px)`).matches,
+    md: window.matchMedia(`(min-width: ${breakpoints.md}px)`).matches,
+    lg: window.matchMedia(`(min-width: ${breakpoints.lg}px)`).matches,
+    xl: window.matchMedia(`(min-width: ${breakpoints.xl}px)`).matches,
   });
 
   const callback = useCallback((e) => {
     switch (e.media) {
-      case `(min-width: ${option.sm}px)`:
-        setMatches({ sm: e.matches });
+      case `(min-width: ${breakpoints.sm}px)`:
+        setMatches((prev) => ({ ...prev, sm: e.matches }));
         break;
-      case `(min-width: ${option.md}px)`:
-        setMatches({ md: e.matches });
+      case `(min-width: ${breakpoints.md}px)`:
+        setMatches((prev) => ({ ...prev, md: e.matches }));
         break;
-      case `(min-width: ${option.lg}px)`:
-        setMatches({ lg: e.matches });
+      case `(min-width: ${breakpoints.lg}px)`:
+        setMatches((prev) => ({ ...prev, lg: e.matches }));
         break;
-      case `(min-width: ${option.xl}px)`:
-        setMatches({ xl: e.matches });
+      case `(min-width: ${breakpoints.xl}px)`:
+        setMatches((prev) => ({ ...prev, xl: e.matches }));
+
         break;
     }
   }, []);
 
-  const mqlSm = useRef(window.matchMedia(`(min-width: ${option.sm}px)`));
-  const mqlMd = useRef(window.matchMedia(`(min-width: ${option.md}px)`));
-  const mqlLg = useRef(window.matchMedia(`(min-width: ${option.lg}px)`));
-  const mqlXl = useRef(window.matchMedia(`(min-width: ${option.xl}px)`));
+  const mqlSm = useRef(window.matchMedia(`(min-width: ${breakpoints.sm}px)`));
+  const mqlMd = useRef(window.matchMedia(`(min-width: ${breakpoints.md}px)`));
+  const mqlLg = useRef(window.matchMedia(`(min-width: ${breakpoints.lg}px)`));
+  const mqlXl = useRef(window.matchMedia(`(min-width: ${breakpoints.xl}px)`));
 
   useEffect(() => {
     mqlSm.current.addListener(callback);
